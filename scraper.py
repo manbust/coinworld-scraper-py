947075b052adf1aa22646fc43bd1f791a8c0595c
manbust <arastoo.dsr@gmail.com>
2025-06-22 22:29:05 -0400

    s

--- scraper.py
+++ scraper.py
@@ -58,6 +58,7 @@ def scrape_trending_tokens(chain: str) -> list:
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.binary_location = "/usr/bin/google-chrome-stable" 
     options.add_argument("--window-size=1920,1080")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
